df2d4608f4f8e500891330cea061d30ccb721894
Onur Çakır <onurcakirkeyif@gmail.com>
2026-03-17 12:32:02 +0000

    File changes

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -39,11 +39,11 @@ export default function About() {
               position: "absolute",
               top: "50%",
               left: "50%",
-              transform: "translate(-50%, -50%) scale(1.1)",
+              transform: "translate(-50%, -45%) scale(1.35)",
               width: "100%",
               height: "100%",
               objectFit: "cover",
-              objectPosition: "center center",
+              objectPosition: "55% 55%",
             }}
           />
           {/* Subtle fade on left edge to blend with page */}
