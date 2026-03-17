77db7900deb13ab8aa79d1921feb51d5c27f5cc4
Onur Çakır <onurcakirkeyif@gmail.com>
2026-03-17 12:55:44 +0000

    File changes

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -5,19 +5,18 @@ const RED = "#B85040";
 
 const projects = [
   { title: "The Mediterranean Seeker – Turkey", client: "Mango", thumbnail: "https://i.ytimg.com/vi_webp/bn1sZ8FxFLo/maxresdefault.webp", url: "https://www.youtube.com/watch?v=bn1sZ8FxFLo" },
-  { title: "The Mediterranean Seeker – The Sea", client: "Mango", thumbnail: "https://i.ytimg.com/vi_webp/Be58H7ZiYDA/maxresdefault.webp", url: "https://www.youtube.com/watch?v=Be58H7ZiYDA" },
+  { title: "One Love", client: "Coca-Cola / Coke Studio", thumbnail: "https://i.ytimg.com/vi/Ul37EtOMh8U/maxresdefault.jpg", url: "https://www.youtube.com/watch?v=Ul37EtOMh8U" },
   { title: "Sundance Villa – Summer Collection", client: "MINDTHEGAP", thumbnail: "https://i.ytimg.com/vi_webp/r2jiGDiULWA/maxresdefault.webp", url: "https://www.youtube.com/watch?v=r2jiGDiULWA" },
+  { title: "Citroën Campaign – Istanbul", client: "Citroën", thumbnail: "https://i.vimeocdn.com/video/2128933241-1f7c34b56d9e108ffb8cf29eef9e87a894c6a17e5ae40d94683465aa89697a18-d?mw=640&q=85", url: "https://vimeo.com/1169974748/e10fd89403" },
+  { title: "Rush – Reality Series", client: "Endemol Shine", thumbnail: "https://i.ytimg.com/vi/OMzsNGfR13Y/maxresdefault.jpg", url: "https://www.youtube.com/watch?v=OMzsNGfR13Y" },
+  { title: "Winter Garden / Kış Bahçesi", client: "Short Film", thumbnail: "https://i.vimeocdn.com/video/1484818990-150c914adcc94693198f4dbf623fbcb7661ec71fac5cb5033dc527a9fff3b3ca-d_640", url: "https://vimeo.com/409533871" },
+  { title: "Turquie, le culte de la beauté?", client: "Arte / Tataki", thumbnail: "https://i.ytimg.com/vi_webp/Mpij5JhuLrI/maxresdefault.webp", url: "https://www.youtube.com/watch?v=Mpij5JhuLrI" },
+  { title: "The Return / Dönüş", client: "Mood Teaser", thumbnail: "https://i.vimeocdn.com/video/1638286094-95a29ed7a97cc8b3ffc60ecd1be707bbf70fd9efd33d2bb17b6436af17b84ab6-d_640", url: "https://vimeo.com/810633919" },
   { title: "Bebemoss Story", client: "Shopify", thumbnail: "https://i.ytimg.com/vi_webp/TpBNceZbHKE/maxresdefault.webp", url: "https://www.youtube.com/watch?v=TpBNceZbHKE" },
   { title: "Humans and AI: Meet Kürşat", client: "Microsoft", thumbnail: "https://i.ytimg.com/vi/wF2CI-AZ19E/maxresdefault.jpg", url: "https://www.youtube.com/watch?v=wF2CI-AZ19E" },
-  { title: "Turquie, le culte de la beauté?", client: "Arte / Tataki", thumbnail: "https://i.ytimg.com/vi_webp/Mpij5JhuLrI/maxresdefault.webp", url: "https://www.youtube.com/watch?v=Mpij5JhuLrI" },
-  { title: "Winter Garden / Kış Bahçesi", client: "Short Film", thumbnail: "https://i.vimeocdn.com/video/1484818990-150c914adcc94693198f4dbf623fbcb7661ec71fac5cb5033dc527a9fff3b3ca-d_640", url: "https://vimeo.com/409533871" },
   { title: "The Lord of the Mussels", client: "The Culture Trip", thumbnail: "https://i.ytimg.com/vi/YniHplbiGnc/maxresdefault.jpg", url: "https://www.youtube.com/watch?v=YniHplbiGnc" },
-  { title: "The Return / Dönüş", client: "Mood Teaser", thumbnail: "https://i.vimeocdn.com/video/1638286094-95a29ed7a97cc8b3ffc60ecd1be707bbf70fd9efd33d2bb17b6436af17b84ab6-d_640", url: "https://vimeo.com/810633919" },
-  { title: "Trial & Triumph: Seven Churches", client: "Appian Media", thumbnail: "https://i.ytimg.com/vi/apMj6heeibo/maxresdefault.jpg", url: "https://www.youtube.com/watch?v=apMj6heeibo" },
-  { title: "One Love", client: "Coca-Cola / Coke Studio", thumbnail: "https://i.ytimg.com/vi/Ul37EtOMh8U/maxresdefault.jpg", url: "https://www.youtube.com/watch?v=Ul37EtOMh8U" },
-  { title: "Rush – Reality Series", client: "Endemol Shine", thumbnail: "https://i.ytimg.com/vi/OMzsNGfR13Y/maxresdefault.jpg", url: "https://www.youtube.com/watch?v=OMzsNGfR13Y" },
-  { title: "Citroën Campaign – Istanbul", client: "Citroën", thumbnail: "https://i.vimeocdn.com/video/2128933241-1f7c34b56d9e108ffb8cf29eef9e87a894c6a17e5ae40d94683465aa89697a18-d?mw=640&q=85", url: "https://vimeo.com/1169974748/e10fd89403" },
   { title: "Raiders of the Lost Past", client: "BBC", thumbnail: "https://i.ytimg.com/vi/CHkIiQ7ghyw/hqdefault.jpg", url: "https://www.youtube.com/watch?v=CHkIiQ7ghyw" },
+  { title: "Trial & Triumph: Seven Churches", client: "Appian Media", thumbnail: "https://i.ytimg.com/vi/apMj6heeibo/maxresdefault.jpg", url: "https://www.youtube.com/watch?v=apMj6heeibo" },
   { title: "Rot Op Naar Je Eigen Land", client: "NPO3", thumbnail: "https://i.ytimg.com/vi/Uqr09TlvVR0/maxresdefault.jpg", url: "https://www.youtube.com/watch?v=Uqr09TlvVR0" },
   { title: "Nuova Hyundai BAYON", client: "Hyundai", thumbnail: "https://i.vimeocdn.com/video/1117256398-c03bc6eb4e362a67880db7eda536ce0f9fd4aaa0eb678bbc85252b6f79402146-d_640", url: "https://vimeo.com/539660481" },
   { title: "Unbound Turkey", client: "Documentary", thumbnail: "https://i.vimeocdn.com/video/2086033384-3dd0afebb066ed0276f1c36b9c3e3b6dc980785fa6fb582f26d3a5ffa3a1d917-d_640", url: "https://vimeo.com/1137882885/118edd0fef" },
@@ -30,6 +29,7 @@ const projects = [
   { title: "Bir Araya Gel", client: "Eskiz", thumbnail: "https://i.ytimg.com/vi/9e1JkoZxGEc/maxresdefault.jpg", url: "https://www.youtube.com/watch?v=9e1JkoZxGEc" },
   { title: "Alo in the Wild: Turkey", client: "Alo Yoga", thumbnail: "https://d357mttm70bw7x.cloudfront.net/normal_19663957-3b41-435a-92fd-d69cd0127d0a.jpg", url: "https://wellnessclub.aloyoga.com/series/alo-in-the-wild-turkey" },
   { title: "Silent House", client: "Mustafa Deniz", thumbnail: "https://images.unsplash.com/photo-1574717025058-2f8737d2e2b7?w=1280&q=80", url: "https://www.mustafadeniz.net/post-production" },
+  { title: "The Mediterranean Seeker – The Sea", client: "Mango", thumbnail: "https://i.ytimg.com/vi_webp/Be58H7ZiYDA/maxresdefault.webp", url: "https://www.youtube.com/watch?v=Be58H7ZiYDA" },
 ];
 
 export default function Portfolio() {
