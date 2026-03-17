7b8c276ebe1baa5c90e61927a95f2610c8fee741
Onur Çakır <onurcakirkeyif@gmail.com>
2026-03-17 10:57:47 +0000

    File changes

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import PageNotFound from './lib/PageNotFound';
 import { AuthProvider, useAuth } from '@/lib/AuthContext';
 import UserNotRegisteredError from '@/components/UserNotRegisteredError';
 import ServiceDetail from './pages/ServiceDetail';
-import Layout from './Layout.jsx';
+import AppLayout from './Layout.jsx';
 
 const { Pages, Layout, mainPage } = pagesConfig;
 const mainPageKey = mainPage ?? Object.keys(Pages)[0];
