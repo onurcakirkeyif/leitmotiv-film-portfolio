430a63c1e0457742579f28ed9ab5e24e9fcd9cc5
Onur Çakır <onurcakirkeyif@gmail.com>
2026-03-17 10:56:38 +0000

    File changes

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import PageNotFound from './lib/PageNotFound';
 import { AuthProvider, useAuth } from '@/lib/AuthContext';
 import UserNotRegisteredError from '@/components/UserNotRegisteredError';
+import ServiceDetail from './pages/ServiceDetail';
+import Layout from './Layout.jsx';
 
 const { Pages, Layout, mainPage } = pagesConfig;
 const mainPageKey = mainPage ?? Object.keys(Pages)[0];
